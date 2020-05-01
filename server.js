63e7427004d84cb03ddf6c7023e82d334ad590c8
Jummay77 <hansangho@yahoo.com>
2020-05-01 16:37:26 -0700

    p2 assessment

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const methodOverride = require('method-override');
-
 const app = express();
+const db = require('./models')
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: false}));
@@ -9,7 +9,40 @@ app.use(express.static('static'));
 app.use(methodOverride('_method'));
 
 // WRITE YOUR ROUTES HERE /////////////////////
+app.get('/', (req, res) => {
+    db.widget.findAll()
+    .then(widgets => {
+        res.render('home', { widgets })
+    })
+    .catch(err => {
+        console.log('Error')
+        res.send('uh oh!')
+    })
+})
 
+app.post('/', (req, res) => {
+    db.widget.create(req.body)
+    .then(newWid => {
+        res.redirect('/')
+    })
+    .catch(err => {
+        console.log('Error:', err)
+        res.send('Uh oh!')
+    })
+})
+
+app.delete('/:id', (req, res)=> {
+    db.widget.destroy({
+        where: {id: req.params.id}
+    })
+    .then(() => {
+        res.redirect('/')
+    })
+    .catch(err=> {
+        console.log('Error in delete', err)
+        res.render('error')
+    })
+})
 
 
 // YOUR ROUTES ABOVE THIS COMMENT /////////////
